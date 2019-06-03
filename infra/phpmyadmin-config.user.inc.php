6caa73ccc3904a7d459cc010613354222f61186a
junjuew <junjuew@cs.cmu.edu>
2019-06-03 13:50:31 -0400

    use phpmyadmin to manage mysql

---
+++ infra/phpmyadmin-config.user.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Enable Zero Configuration for storage 
+To enable phpmyadmin's capabilities to bookmark queries,
+go to operation tab with a database selected.
+There is a option to create a pma table in the database
+which stores all phpmyadmin(pma) data.
+*/
+$cfg['ZeroConf'] = true;
