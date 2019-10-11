95e5bd4da91cf70a2ec5c19166c9dc130b15d905
junjuew <junjuew@cs.cmu.edu>
2019-10-11 17:21:03 -0400

    [fileutils] update fileutils to handle video extraction and resize better

--- rmexp/dataset/fileutils.py
+++ rmexp/dataset/fileutils.py
@@ -1,8 +1,12 @@
 #! /usr/bin/env python
 
-import os
+import functools
 import glob
+import importlib
+import multiprocessing
+import os
 import shutil
+
 import fire
 from logzero import logger
 
@@ -28,7 +32,7 @@ def rename_files_in_directory_to_sequence(dir_path, ext='jpg'):
     file_paths = sorted(glob.glob(os.path.join(bk_dir_path, '*.' + ext)))
     for idx, file_path in enumerate(file_paths):
         shutil.copy(file_path, os.path.join(
-            dir_path, '{:010d}'.format(idx+1) + '.' + ext))
+            dir_path, '{:010d}'.format(idx + 1) + '.' + ext))
     shutil.rmtree(bk_dir_path)
 
 
@@ -62,7 +66,7 @@ def rename_default_trace(dir_path):
     import cv2
     import shutil
     trace_num_min, trace_num_max = _get_max_trace_num(dir_path)
-    for i in range(trace_num_min, trace_num_max+1):
+    for i in range(trace_num_min, trace_num_max + 1):
         cur_default_trace = os.path.join(dir_path, str(i), 'video.mp4')
         if os.path.exists(cur_default_trace) and not os.path.islink(cur_default_trace):
             res = get_video_resolution(cur_default_trace)
@@ -76,19 +80,20 @@ def rename_default_trace(dir_path):
             shutil.move(cur_default_trace, new_path)
 
 
-def _get_highest_res_trace(dir_path):
-    import glob
-    trace_num_min, trace_num_max = _get_max_trace_num(dir_path)
-    highest_res_traces = []
-    for i in range(trace_num_min, trace_num_max+1):
-        pat = os.path.join(dir_path, str(i), 'video-*mp4')
-        all_traces = glob.glob(pat)
-        trace_ress = [max(get_video_resolution(trace)) for trace in all_traces]
-        assert len(
-            trace_ress) > 0, 'dir_path: {} does not have traces'.format(pat)
-        highest_res_traces.append(os.path.join(
-            dir_path, str(i), 'video-{}.mp4'.format(max(trace_ress))))
-    return highest_res_traces
+def _get_highest_resolution_trace_path(trace_dir_path, trace_pattern='video*mp4'):
+    """Return the video path with the highest resolution.
+
+    Highest is defined by the max of width and height.
+    """
+    pat = os.path.join(trace_dir_path, trace_pattern)
+    trace_paths = glob.glob(pat)
+    assert len(
+        trace_paths) > 0, 'dir_path: {} does not have traces'.format(pat)
+    # (max(width, height), trace_path)
+    path_resolution_tuples = [(max(get_video_resolution(trace_path)), trace_path) for
+                              trace_path in trace_paths]
+    path_resolution_tuples.sort(key=lambda x: x[0], reverse=True)
+    return path_resolution_tuples[0][1]
 
 
 def resize_trace(input_path, output_path, width):
@@ -103,53 +108,72 @@ def resize_trace(input_path, output_path, width):
             'Cmd Error: {} has return code {}'.format(cmd, p.returncode))
 
 
-def correct_trace_resolution(app, dir_path, force=False):
+def _get_trace_dir_path(app_dataset_dir, trace_ids):
+    if trace_ids is None:
+        paths = [os.path.join(app_dataset_dir, trace_id)
+                 for trace_id in os.listdir(app_dataset_dir)
+                 if os.path.isdir(os.path.join(app_dataset_dir, trace_id))]
+    else:
+        paths = [os.path.join(app_dataset_dir, str(trace_id))
+                 for trace_id in trace_ids if os.path.isdir(os.path.join(app_dataset_dir, str(trace_id)))]
+    return paths
+
+
+def correct_app_dataset_resolution(app, app_dataset_dir_path, trace_ids=None, force=False):
     """Correct default video trace resolution based on app setting.
     It will take the max resolution video it finds, scale it 
     to the desired resolution, and create a symlink.
 
     app: app name
-    dir_path: trace directory of app. each trace should be named in
-    file directories 0,1,2...
+    app_dataset_dir_path: trace directory of the app. Within this directory each should be directories
+    for each trace e.g. 0,1,2.
+    trace_ids: a list of trace directory names. The convention is to use the id (e.g. 0, 1, 2)
+    as the directory name.
     """
-    import importlib
+    if trace_ids is not None:
+        assert type(
+            trace_ids) == list, 'trace_ids should be a list of trace directory names not {}'.format(
+                type(trace_ids))
+
     app = importlib.import_module(app)
-    candidate_traces = _get_highest_res_trace(dir_path)
-    # resize traces
-    for trace in candidate_traces:
-        base_dir = os.path.dirname(trace)
-        actual_trace = os.path.basename(trace)
-        default_trace = os.path.join(base_dir, 'video.mp4')
-        logger.debug('working on dir {}...'.format(base_dir))
+    trace_dir_paths = _get_trace_dir_path(app_dataset_dir_path, trace_ids)
+
+    # resize traces in trace_dir_path
+    for trace_dir_path in trace_dir_paths:
+        logger.debug('working on trace directory {}...'.format(trace_dir_path))
+        trace_path = _get_highest_resolution_trace_path(trace_dir_path)
+        trace_file_name = os.path.basename(trace_path)
+        default_trace_path = os.path.join(trace_dir_path, 'video.mp4')
 
         # check if existing video.mp4 satisfies the requirements
-        if os.path.islink(default_trace):
+        if os.path.islink(default_trace_path):
             if force:
-                os.unlink(default_trace)
+                os.unlink(default_trace_path)
             else:
-                shape = get_video_resolution(default_trace)
+                shape = get_video_resolution(default_trace_path)
                 if max(shape) <= app.config.IMAGE_MAX_WH:
                     continue
-                os.unlink(default_trace)
+                os.unlink(default_trace_path)
 
-        if max(get_video_resolution(trace)) > app.config.IMAGE_MAX_WH:
-            output_trace = os.path.join(
-                base_dir, 'video-{}.mp4'.format(app.config.IMAGE_MAX_WH))
-            resize_trace(trace, output_trace, app.config.IMAGE_MAX_WH)
-            actual_trace = 'video-{}.mp4'.format(app.config.IMAGE_MAX_WH)
+        if max(get_video_resolution(trace_path)) > app.config.IMAGE_MAX_WH:
+            resized_trace_path = os.path.join(
+                trace_dir_path, 'video-{}.mp4'.format(app.config.IMAGE_MAX_WH))
+            resize_trace(trace_path, resized_trace_path,
+                         app.config.IMAGE_MAX_WH)
+            trace_file_name = 'video-{}.mp4'.format(app.config.IMAGE_MAX_WH)
 
         # create link
-        if os.path.exists(default_trace):
-            if not os.path.islink(default_trace):
-                raise ValueError('{} is not a link'.format(default_trace))
-        os.symlink(actual_trace, default_trace)
+        if os.path.exists(default_trace_path):
+            if not os.path.islink(default_trace_path):
+                raise ValueError('{} is not a link'.format(default_trace_path))
+        os.symlink(trace_file_name, default_trace_path)
 
 
 def decode_to_imgs_trace_dir(dir_path, force=False,
                              video_fname='video.mp4',
                              output_image_dname='video-images'
                              ):
-    """Extract a video.mpy in a trace directory to a dir of images."""
+    """Extract a video.mp4 in a trace directory to a dir of images."""
     import subprocess
     video_fp = os.path.join(dir_path, video_fname)
     if not os.path.exists(video_fp):
@@ -177,12 +201,19 @@ def decode_to_imgs_trace_dir(dir_path, force=False,
             'Cmd Error: {} has return code {}'.format(cmd, p.returncode))
 
 
-def decode_to_imgs_app_dataset_dir(dir_path, force=False):
+def decode_video_to_imgs_in_app_dataset(app_dataset_dir_path, trace_ids=None, force=False):
     """Extract all traces in dir_path to images."""
-    import subprocess
-    trace_num_min, trace_num_max = _get_max_trace_num(dir_path)
-    for i in range(trace_num_min, trace_num_max+1):
-        decode_to_imgs_trace_dir(os.path.join(dir_path, str(i)), force=force)
+    if trace_ids is not None:
+        assert type(
+            trace_ids) == list, 'trace_ids should be a list of trace directory names not {}'.format(
+                type(trace_ids))
+
+    worker_pool = multiprocessing.Pool(10)
+    trace_dir_paths = _get_trace_dir_path(app_dataset_dir_path, trace_ids)
+    job = functools.partial(decode_to_imgs_trace_dir, force=force)
+    worker_pool.map_async(job, trace_dir_paths)
+    worker_pool.close()
+    worker_pool.join()
 
 
 def get_image_sequence_resolution(image_sequence_path):
@@ -200,7 +231,7 @@ def get_dataset_stats(app, dir_path, store=False):
     from rmexp.schema import models
     import cv2
     trace_num_min, trace_num_max = _get_max_trace_num(dir_path)
-    for i in range(trace_num_min, trace_num_max+1):
+    for i in range(trace_num_min, trace_num_max + 1):
         default_trace = os.path.join(dir_path, str(i), 'video-images')
         resolution = get_image_sequence_resolution(default_trace)
         frames = len(glob.glob(os.path.join(default_trace, '*.jpg')))
